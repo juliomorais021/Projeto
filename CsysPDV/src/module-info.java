7472acaa36aab55a94ecb7a5dc1e82b59bc2d6b8
FilipeHSAraujo <84210747+FilipeHSAraujo@users.noreply.github.com>
2021-09-28 16:56:06 -0300

    pre-alpha 0.0.1
     Features:
      -Pre-Alfa GUI
      -program package
      -image package ( on coming)
    
    Signed-off-by: FilipeHSAraujo <84210747+FilipeHSAraujo@users.noreply.github.com>

--- CsysPDV/src/module-info.java
+++
@@ -1,2 +0,0 @@
-module projeto_faculdade {
-}
---
+++ CsysPDV/src/program/ESTOQUE.java
@@ -0,0 +1,94 @@
+package program;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JSplitPane;
+import java.awt.BorderLayout;
+import javax.swing.JButton;
+import javax.swing.SwingConstants;
+import javax.swing.JPanel;
+import javax.swing.JSeparator;
+import javax.swing.JLabel;
+import javax.swing.Box;
+import java.awt.Panel;
+import java.awt.Component;
+import java.awt.Color;
+import javax.swing.JTable;
+import javax.swing.JTextArea;
+import java.awt.Scrollbar;
+
+public class ESTOQUE {
+
+	private JFrame frmEstoque;
+	private JTable table;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ESTOQUE window = new ESTOQUE();
+					window.frmEstoque.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public ESTOQUE() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmEstoque = new JFrame();
+		frmEstoque.setTitle("Estoque");
+		frmEstoque.setAutoRequestFocus(false);
+		frmEstoque.setBounds(100, 100, 600, 450);
+		frmEstoque.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmEstoque.getContentPane().setLayout(null);
+		
+		JButton btnNewButton = new JButton("Adicionar");
+		btnNewButton.setBounds(10, 47, 83, 31);
+		frmEstoque.getContentPane().add(btnNewButton);
+		
+		JButton btnRemover = new JButton("Remover");
+		btnRemover.setBounds(10, 89, 83, 31);
+		frmEstoque.getContentPane().add(btnRemover);
+		
+		JButton btnEditar = new JButton("Editar");
+		btnEditar.setBounds(10, 131, 83, 31);
+		frmEstoque.getContentPane().add(btnEditar);
+		
+		table = new JTable();
+		table.setBounds(103, 47, 424, 312);
+		frmEstoque.getContentPane().add(table);
+		
+		JLabel lblNewLabel = new JLabel("ID");
+		lblNewLabel.setBounds(115, 24, 46, 14);
+		frmEstoque.getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Nome");
+		lblNewLabel_1.setBounds(187, 24, 46, 14);
+		frmEstoque.getContentPane().add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Pre\u00E7o");
+		lblNewLabel_2.setBounds(302, 24, 46, 14);
+		frmEstoque.getContentPane().add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("Quantidade");
+		lblNewLabel_3.setBounds(402, 22, 83, 14);
+		frmEstoque.getContentPane().add(lblNewLabel_3);
+	}
+}
